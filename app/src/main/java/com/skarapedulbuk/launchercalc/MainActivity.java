c7471bd197fc4adb257dc53a1485eebcb5efaf4f
skarapedulbuk <voronezh2@gmail.com>
2021-10-20 16:46:59 +0300

    3. * Сделайте интент-фильтр для запуска калькулятора извне, а также напишите тестовое приложение, запускающее приложение-калькулятор.

--- app/src/main/java/com/skarapedulbuk/launchercalc/MainActivity.java
+++ app/src/main/java/com/skarapedulbuk/launchercalc/MainActivity.java
@@ -2,6 +2,8 @@ package com.skarapedulbuk.launchercalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(v -> {
+            Uri uri = Uri.parse("calc://start");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(Intent.createChooser(intent, getString(R.string.some_message)));
+
+
+        });
     }
 }
